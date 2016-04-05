a1f0964cdef93e4ddb83542dbc126bfd186a24bc
shadowhide50@gmail.com <shadowhide50@gmail.com>
2016-04-05 20:38:20 +0500

    helper function for working with files

--- service/models/File.php
+++ service/models/File.php
@@ -15,6 +15,15 @@ class File {
         }
         $handle = fopen($filePath, 'r');
         return $handle;
+    }
+    
+    public static function getFullPathMetadata($fileName) {
+        $pathMetadata = \Yii::$app->params['metadataFolder'];
+        return $pathMetadata . '/' . $fileName;
     }            
     
+    public static function getFullPathFile($fileName) {
+        $dir = \Yii::$app->params['dataFolder'];
+        return $dir . '/' . $fileName;
+    }
 }
