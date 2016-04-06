0b152174b6f035415574bc89a5978de28ca1c914
shadowhide50@gmail.com <shadowhide50@gmail.com>
2016-04-06 21:17:03 +0500

    initialization for unit test

--- service/tests/codeception/config/params.php
+++ service/tests/codeception/config/params.php
@@ -1,5 +1,6 @@
 <?php
 
+// if you change these params, change path in unit/_bootstrap for initialization folder fo unit test
 return [
     'dataFolder' => __DIR__ . '/../dataTest/files',
     'metadataFolder' => __DIR__ . '/../dataTest/metadata',
--- service/tests/codeception/unit/_bootstrap.php
+++ service/tests/codeception/unit/_bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
 // add unit testing specific bootstrap code here
+$testDataFolder = __DIR__ . '/../dataTest/files';
+$testMetadataFolder = __DIR__ . '/../dataTest/metadata';
+
+if (!file_exists($testDataFolder)) {
+    mkdir($testDataFolder, 0777, TRUE);
+}
+
+if (!file_exists($testMetadataFolder)) {
+    mkdir($testMetadataFolder, 0777, TRUE);
+}
