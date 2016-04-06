ae69fb010459014e6d9cee510734c0e51016147b
shadowhide50@gmail.com <shadowhide50@gmail.com>
2016-04-06 21:02:54 +0500

    get metadata file for fileName, save metadata file in FileRepositoryFS

--- service/models/data/FileRepositoryFS.php
+++ service/models/data/FileRepositoryFS.php
@@ -4,15 +4,41 @@ namespace app\models\data;
 
 use app\models\data\IFileRepository;
 use app\models\File;
+use app\models\FileMetadata;
 
 class FileRepositoryFS implements \app\models\data\IFileRepository {
     
     public static function getFileMetadata($fileName, $userId) {
         $pathMetadata = File::getFullPathMetadata($fileName);
+        assert('!is_null($pathMetadata)', 'Check params metadataFolder');
         
-        
-        
-        return null;
+        $metadata = FileRepositoryFS::loadFileMetadata($pathMetadata);
+        if (is_null($metadata)) {
+            return NULL;
+        }
+        if ($userId === $metadata->Owner) {
+            return $metadata;
+        }
+        else {
+            throw new AccessDenied();
+        }
+    }
+    
+    public static function saveFileMetadata($metadata) {
+        $pathMetadata = File::getFullPathMetadata($metadata->Name);
+        assert('!is_null($pathMetadata)', 'Check params metadataFolder');
+        $jsonMetadata = json_encode($metadata);
+        $handle = fopen($pathMetadata, 'w');
+        if (is_null($handle)) {
+            return FALSE;
+        }
+        if (!fwrite($handle, $jsonMetadata)) {
+            fclose($handle);
+            return FALSE;
+        }
+        fflush($handle);
+        fclose($handle);
+        return TRUE;
     }
     
     public static function getFiles($userId) {
@@ -21,5 +47,16 @@ class FileRepositoryFS implements \app\models\data\IFileRepository {
     
     public static function createFile($fileMetadata, $userId) {
         return null;
+    }
+    
+    private static function loadFileMetadata($fullMetadataPath) {
+        if (!file_exists($fullMetadataPath)) {
+            return NULL;
+        }
+        $handle = fopen($fullMetadataPath, "r");
+        $jsonMetadata = fgets($handle);
+        fclose($handle);
+        $metadata = new FileMetadata(json_decode($jsonMetadata, TRUE));
+        return $metadata;
     }
 }
